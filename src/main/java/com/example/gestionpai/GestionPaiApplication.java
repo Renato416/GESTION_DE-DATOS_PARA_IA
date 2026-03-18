99bfebd9b63e92172d4061954c1026fc31fbe05c
VA- Alumno <VA-Alumno@duoc.acad>
2026-03-18 18:08:30 -0300

    Initial commit

---
+++ src/main/java/com/example/gestionpai/GestionPaiApplication.java
@@ -0,0 +1,13 @@
+package com.example.gestionpai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionPaiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionPaiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gestionpai/GestionPaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gestionpai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionPaiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
